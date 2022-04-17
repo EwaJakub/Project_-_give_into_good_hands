f13f6ed55fb28505be1caa6159a2bf6cd95f3784
EwaJakub <ewa.jak@onet.pl>
2022-04-17 18:34:07 +0200

    langing page, redirections

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import View
+from random import sample
+
 from .models import Category, Donation, Institution
 
 # Create your views here.
 
 
-# Langing Page View
+"""
+Langing Page View, counts all quantity of donations (bags sum) and all donated institutions.
+Shows three insitutions added to base of application, splitted on base of institutions types on each view.
+"""
 class LandingPage(View):
     template_name = 'main_app/index.html'
 
     def get(self, request):
         donation = sum(Donation.objects.all().values_list('quantity', flat=True))
-        institution = len(Institution.objects.all())
+        # show all of supported institutions, not only added -->
+        institution = len(set(Donation.objects.all().values_list('institution', flat=True)))
+        # institution = len(Institution.objects.all()) --> show all added institutions
+        foundations = Institution.objects.filter(type=1)
+        organizations = Institution.objects.filter(type=2)
+        local_collections = Institution.objects.filter(type=3)
         ctx = {
             'donation': donation,
-            'institution': institution
+            'institution': institution,
+            'foundations': foundations,
+            'organizations': organizations,
+            'local_collections': local_collections,
         }
         return render(request, self.template_name, ctx)
 
